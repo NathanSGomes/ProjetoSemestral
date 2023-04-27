d32617e7a941260ef2dac282d5776c8f7cdf8d74
Nathan Gomes <91997214+NathanSGomes@users.noreply.github.com>
2023-04-27 11:09:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -19,3 +19,12 @@ function ocultar(){
 };
 
 ocultar();
+
+
+const cards = document.querySelectorAll(".card__inner");
+
+function flipCard() {
+    this.classList.toggle('is-flipped');
+};
+
+cards.forEach((card) =>card.addEventListener("click", flipCard));
